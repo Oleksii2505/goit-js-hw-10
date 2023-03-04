8cdf6c307c7856ae54eea1fe2befc6be9094cb97
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-03-04 10:03:37 +0200

    Done first part

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
+
+let searchCountryName = '';
+
+input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange() {}
