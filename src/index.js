1b5dbb2ad7c83347e67e4f3f56e0449a134b9b8e
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-03-04 19:07:27 +0200

    Add function

--- src/index.js
+++ src/index.js
@@ -14,3 +14,41 @@ let searchCountryName = '';
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 function onInputChange() {}
+
+function renderCountryCard() {
+  clearAll();
+  const c = country[0];
+  const renderCard = `<div class="country-card">
+  <div class="country-card__hero">
+    <img src="${c.flags.svg}" alt="Flag" width="50" height="30" />
+    <h2 class="country-card__name">${c.name.official}</h2>
+  </div>
+  <p class="country-card__text">
+    Capital: <span class="value">${c.capital}</span>
+  </p>
+  <p class="country-card__text">
+    Population: <span class="value">${c.population}</span>
+  </p>
+  <p class="country-card__text">
+    Languages:
+    <span class="value">${Object.values(c.languages).join(',')}</span>
+  </p>
+</div>`;
+  div.innerHTML = renderCard;
+}
+function renderCountryList() {
+  clearAll();
+  const renderList = country
+    .map(
+      c => `<li class="country-list__item">
+  <img src="${c.flags.svg}" alt="Flag" width="50" height="30" />
+  <h2 class="country-card__name">${c.name.official}</h2>
+  </li>`
+    )
+    .join('');
+  list.insertAdjacentHTML('beforeend', renderList);
+}
+function clearAll() {
+  list.innerHTML = '';
+  div.innerHTML = '';
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,7 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 export function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (response.ok) {
       return response.json();
