47e681e451f1aba91899bd66dc4f12aaad55ef9c
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-03-05 17:20:37 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -13,14 +13,37 @@ let searchCountryName = '';
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
-function onInputChange() {}
+function onInputChange() {
+  searchCountryName = input.value.trim();
+  if (searchCountryName === '') {
+    clearAll();
+    return;
+  } else
+    fetchCountries(searchCountryName)
+      .then(countryNames => {
+        if (countryNames.length < 2) {
+          renderCountryCard(countryNames);
+        } else if (countryNames.length < 10 && countryNames.length > 1) {
+          renderCountryList(countryNames);
+        } else {
+          clearAll();
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(() => {
+        clearAll();
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+}
 
-function renderCountryCard() {
+function renderCountryCard(country) {
   clearAll();
   const c = country[0];
   const renderCard = `<div class="country-card">
   <div class="country-card__hero">
-    <img src="${c.flags.svg}" alt="Flag" width="50" height="30" />
+    <img src="${c.flags.svg}" alt="Flag" width="60" height="40" />
     <h2 class="country-card__name">${c.name.official}</h2>
   </div>
   <p class="country-card__text">
@@ -36,18 +59,20 @@ function renderCountryCard() {
 </div>`;
   div.innerHTML = renderCard;
 }
-function renderCountryList() {
+
+function renderCountryList(country) {
   clearAll();
   const renderList = country
     .map(
       c => `<li class="country-list__item">
   <img src="${c.flags.svg}" alt="Flag" width="50" height="30" />
-  <h2 class="country-card__name">${c.name.official}</h2>
+  <h2 class="country-list__name">${c.name.official}</h2>
   </li>`
     )
     .join('');
-  list.insertAdjacentHTML('beforeend', renderList);
+  list.innerHTML = renderList;
 }
+
 function clearAll() {
   list.innerHTML = '';
   div.innerHTML = '';
